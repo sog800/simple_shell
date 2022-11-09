ac79058358e43f282eb6ab47e62f5e1106d66cae
Cyrus <daniellongwe800@gmail.com>
2022-11-09 07:36:57 -0800

    Code that pasess Bett

---
+++ betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - clean code for betty
+ *
+ * Return: Allways return 0
+ */
+
+int main(void)
+{
+	char *betty;
+
+	betty = "Beautifull Betty Code";
+	printf("%s\n", betty);
+
+	return (0);
+}
